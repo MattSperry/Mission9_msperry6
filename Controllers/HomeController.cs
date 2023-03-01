c6c8d8de1d566990e6590d840c1a0f37eebba1cd
MattSperry <matthewjoelsperry@gmail.com>
2023-03-01 14:55:31 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9_msperry6.Models;
+using Mission9_msperry6.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_msperry6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookRepository repo;
+        
+        public HomeController (BookRepository temp)
+        {
+            repo = temp;
+        }
+        //private BookstoreContext context { get; set; }
+
+        //public HomeController(BookstoreContext temp) => context = temp;
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Models/EFBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_msperry6.Models
+{
+    public class EFBookRepository : BookRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
